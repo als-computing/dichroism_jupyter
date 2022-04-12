23299170afacf9ab3a10363457d5bb0df3f4dc04
Padraic Shafer <PShafer@lbl.gov>
2022-04-12 15:26:44 -0700

    Install packaged module in conda env

--- dxr_env/dichroism-jupyter/install_proj_pkgs.sh
+++ dxr_env/dichroism-jupyter/install_proj_pkgs.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -euo pipefail
 
-# python -m pip install ./dist/"${MODULE_PROJ_NAME}"-*.tar.gz
-pip install ./dist/"${MODULE_PROJ_NAME}"-*.tar.gz
+python -m pip install ./tmp/dist/"${MODULE_PROJ_NAME}"-*.tar.gz
+# pip install ./tmp/dist/"${MODULE_PROJ_NAME}"-*.tar.gz
