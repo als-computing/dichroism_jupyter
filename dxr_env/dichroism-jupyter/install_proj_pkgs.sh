75bd374c3c8b1a92be0825932f5a6b556563f840
Padraic Shafer <PShafer@lbl.gov>
2022-04-12 12:35:57 -0700

    Do not use 'python -m' with pip to install packaged module

--- dxr_env/dichroism-jupyter/install_proj_pkgs.sh
+++ dxr_env/dichroism-jupyter/install_proj_pkgs.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 set -euo pipefail
 
-python -m pip install ./dist/"${MODULE_PROJ_NAME}"-*.tar.gz
+# python -m pip install ./dist/"${MODULE_PROJ_NAME}"-*.tar.gz
+pip install ./dist/"${MODULE_PROJ_NAME}"-*.tar.gz
