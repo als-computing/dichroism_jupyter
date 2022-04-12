bca83d2a5c35dcdb356c4f8aabc168feef480e70
Padraic Shafer <PShafer@lbl.gov>
2022-04-12 11:34:29 -0700

    Use 'python -m' with pip

--- dxr_env/dichroism-jupyter/install_proj_pkgs.sh
+++ dxr_env/dichroism-jupyter/install_proj_pkgs.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 set -euo pipefail
 
-# python -m pip install ./dist/"${MODULE_PROJ_NAME}"-*.tar.gz
-pip install ./dist/"${MODULE_PROJ_NAME}"-*.tar.gz
+python -m pip install ./dist/"${MODULE_PROJ_NAME}"-*.tar.gz
